2d4762c1b8d57e3d8d7b01bc0c4cb91bbf2cca26
imananoosheh <imananoosheh@gmail.com>
2018-07-12 22:34:39 +0430

    landing page design , JS implem. for each senario (coded in HPload.js, means High Priority load)

---
+++ js/HPlaod.js
@@ -0,0 +1,10 @@
+function landingpage2Services() {
+    document.getElementById('landingpage').style.display = 'none';
+    location.hash = "#services";
+    document.getElementById('navbar').style.display = 'flex';
+}
+function landingpage2Head() {
+    document.getElementById('landingpage').style.display = 'none';
+    location.hash = "#";
+    document.getElementById('navbar').style.display = 'flex';
+}
--- js/mainCR.js
+++ js/mainCR.js
@@ -1,6 +1,7 @@
-function xYZ (){
+function xYZ() {
     var replacement = '';
     replacement += '<div>| Designed & Developed by API Team © 2018 | All rights reserved. |</div>';
     return replacement;
 }
 $('#copyright').empty().html(xYZ());
+
