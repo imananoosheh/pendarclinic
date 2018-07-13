2407d107c5884b8f37579d988820b34f73f31094
imananoosheh <imananoosheh@gmail.com>
2018-07-13 10:03:12 +0430

    Up button added and is sync with landing page

--- js/mainCR.js
+++ js/mainCR.js
@@ -5,3 +5,24 @@ function xYZ() {
 }
 $('#copyright').empty().html(xYZ());
 
+//Go to the Top Button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 && document.getElementById("landingpage").style.display == "none") {
+      document.getElementById("gototopbtn").style.display = "block";
+    } else {
+      document.getElementById("gototopbtn").style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    $("html, body").animate({ scrollTop: 0 }, 600);  
+    //document.body.scrollTop = 0;
+    //document.documentElement.scrollTop = 0;
+  }
+  
