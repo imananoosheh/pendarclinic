8abe007165912164dc028a0474c9ca8edc45c31d
imananoosheh <imananoosheh@gmail.com>
2018-07-16 11:16:40 +0430

    Service button in landing fixed.

--- js/HPlaod.js
+++ js/HPlaod.js
@@ -1,10 +1,3 @@
 function landingpage2Services() {
-    document.getElementById('landingpage').style.display = 'none';
-    location.hash = "#services";
-    document.getElementById('navbar').style.display = 'flex';
-}
-function landingpage2Head() {
-    document.getElementById('landingpage').style.display = 'none';
-    location.hash = "#";
-    document.getElementById('navbar').style.display = 'flex';
+    window.location = 'main.html#service';
 }
